2199f8c284ef6f7f3c376ff3b5977fc0919aa2a9
azoom-dang-van-hoi <dang.van.hoi@azoom.jp>
2023-11-14 17:34:55 +0700

    :+1: Test build lib

--- routes/users/post.js
+++ routes/users/post.js
@@ -17,6 +17,7 @@ export const apiDefinition = {
   response: UserSchema,
   errorStatuses: [400, 403, 404, 500],
 }
+
 export default (req, res) => {
   return res.send({
     id: Math.floor(Math.random() * 1000000),
